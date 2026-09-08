ca51ee1f2b66bb7359a28d546a88feba7adf3097
AleSchargro <alejo-schargro@hotmail.com>
2026-09-07 23:17:13 -0300

    Actualizaciones

--- data/puzzles.js
+++ data/puzzles.js
@@ -5,10 +5,10 @@
 const PUZZLES = {
   "2026-09-04": {
     categories: [
-      { name: "Clásicos del fútbol", words: ["RIVER", "BOCA", "RACING", "INDEPENDIENTE"] },
-      { name: "Comida de domingo", words: ["ASADO", "LOCRO", "EMPANADA", "MILANESA"] },
-      { name: "Provincias del norte", words: ["SALTA", "CHACO", "FORMOSA", "MISIONES"] },
-      { name: "Cosas que se toman", words: ["MATE", "FERNET", "TERMO", "SODA"] }
+      { name: "Si estás leyendo esto, avisale a Alejo!", words: ["PRUEBA 1", "PRUEBA 2", "PRUEBA 3", "PRUEBA 4"] },
+      { name: "Categoría Prueba 2", words: ["PRUEBA 5", "PRUEBA 6", "PRUEBA 7", "PRUEBA 8"] },
+      { name: "Categoría Prueba 3", words: ["PRUEBA 9", "PRUEBA 10", "PRUEBA 11", "PRUEBA 12"] },
+      { name: "Categoría Prueba 4", words: ["PRUEBA 13", "PRUEBA 14", "PRUEBA 15", "PRUEBA 16"] }
     ]
   },
   "2026-09-05": {
@@ -33,6 +33,30 @@ const PUZZLES = {
       { name: "Golosinas tradicionales", words: ["Alfajor", "Turrón", "Rhodesia", "DRF"] },
       { name: "Elementos típicos de gaucho", words: ["Poncho", "Rebenque", "Guitarra", "Rastra"] },
       { name: "Nombre de cantantes de Tango", words: ["Tita", "Roberto", "Carlos", "Anibal"] }
+    ]
+  },
+  "2026-09-08": {
+    categories: [
+      { name: "Del lunfardo", words: ["Baranda", "Berreta", "Minga", "Macana"] },
+      { name: "Profesional del derecho", words: ["Jurista", "Legista", "Letrado", "Defensor"] },
+      { name: "Que aparecen en una carta", words: ["Ñoquis", "Ensalada Caesar", "Firma", "Destinatario"] },
+      { name: "Preposición + Corte de carne", words: ["Sobre Vacío", "Contra Bife", "Vía Lomo", "Hasta Falda"] }
+    ]
+  },
+  "2026-09-09": {
+    categories: [
+      { name: "Si estás leyendo esto, avisale a Alejo!", words: ["PRUEBA 1", "PRUEBA 2", "PRUEBA 3", "PRUEBA 4"] },
+      { name: "Categoría Prueba 2", words: ["PRUEBA 5", "PRUEBA 6", "PRUEBA 7", "PRUEBA 8"] },
+      { name: "Categoría Prueba 3", words: ["PRUEBA 9", "PRUEBA 10", "PRUEBA 11", "PRUEBA 12"] },
+      { name: "Categoría Prueba 4", words: ["PRUEBA 13", "PRUEBA 14", "PRUEBA 15", "PRUEBA 16"] }
+    ]
+  },
+  "2026-09-10": {
+    categories: [
+      { name: "Si estás leyendo esto, avisale a Alejo!", words: ["PRUEBA 1", "PRUEBA 2", "PRUEBA 3", "PRUEBA 4"] },
+      { name: "Categoría Prueba 2", words: ["PRUEBA 5", "PRUEBA 6", "PRUEBA 7", "PRUEBA 8"] },
+      { name: "Categoría Prueba 3", words: ["PRUEBA 9", "PRUEBA 10", "PRUEBA 11", "PRUEBA 12"] },
+      { name: "Categoría Prueba 4", words: ["PRUEBA 13", "PRUEBA 14", "PRUEBA 15", "PRUEBA 16"] }
     ]
   }
 };
